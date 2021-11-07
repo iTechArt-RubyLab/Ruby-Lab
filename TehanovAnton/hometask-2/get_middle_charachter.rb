e9d3d5b0c1009326762ce142c04a96f158d1bcca
anton <tehanovanton@gmail.com>
2021-11-07 19:06:02 +0300

    run_cli method

--- TehanovAnton/hometask-2/get_middle_charachter.rb
+++ TehanovAnton/hometask-2/get_middle_charachter.rb
@@ -1,6 +1,32 @@
+#!/usr/bin/env ruby
 # frozen_string_literal: true
+
+ERROR_MESSAGE = "String can't be blank"
+ENTER_PROMPT = 'Enter string:'
+EXIT = 'exit!'
 
 def get_middle(string)
   half = string.size / 2
   string.length.odd? ? string[half] : string[(half - 1)..half]
 end
+
+def error_message
+  ERROR_MESSAGE
+end
+
+def result(input)
+  "Middle character: #{get_middle(input)}"
+end
+
+def run_cli
+  loop do
+    puts ENTER_PROMPT
+    input = gets.chomp
+
+    exit if input == EXIT
+
+    puts input.empty? ? error_message : result(input)
+  end
+end
+
+run_cli
