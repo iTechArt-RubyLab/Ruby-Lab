7d4576f49fa07d088d164682e14a554767ef54f1
Nastya <Nastassia.Bandarenka@itechart-group.com>
2021-11-09 13:17:07 +0200

    Change the name of the variable in the method

--- NastassiaBon/hometask-2/get_middle_charachter.rb
+++ NastassiaBon/hometask-2/get_middle_charachter.rb
@@ -1,30 +1,29 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-def get_middle(s)
-    middle = s.length / 2 #your code here
-    
-    if s.length.odd?
-      s[middle]
-    else
-      s[middle - 1] + s[middle]
-    end
-    
-  end
+def get_middle(string)
+  middle = string.length / 2 # your code here
 
-  def run_cli
-    loop do 
-    puts "Enter string: "
+  if string.length.odd?
+    string[middle]
+  else
+    string[middle - 1] + string[middle]
+  end
+end
+
+def run_cli
+  loop do
+    puts 'Enter string: '
     string = gets.chomp
-    
+
     break if string == 'exit'
 
     if string.empty?
-        puts 'String can not be blank!'
-    else 
-        puts "Middle character: #{get_midddle(string)}"
-    end 
-end
+      puts 'String can not be blank!'
+    else
+      puts "Middle character: #{get_middle(string)}"
+    end
+  end
 end
 
-run_cli 
+run_cli
