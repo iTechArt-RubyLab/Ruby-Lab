03e86b069fa47a0ffd1f2691b7ebbe81c56ef609
mlshvch <malashevichdev@gmail.com>
2021-11-09 16:16:00 +0300

    Add run_cli function

--- mlshvch/hometask-2/get_middle_character.rb
+++ mlshvch/hometask-2/get_middle_character.rb
@@ -5,3 +5,19 @@ def get_middle(string)
   mid = len / 2
   len.even? == true ? string[mid - 1, mid] : string[mid]
 end
+
+def run_cli
+  loop do
+    puts 'Enter string: '
+    input = gets.chomp
+    break if input == 'exit!'
+
+    if input.empty? == true
+      puts 'String can not be blank!'
+    else
+      puts "Middle character: #{get_middle(input)}"
+    end
+  end
+end
+
+run_cli
