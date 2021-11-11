bbbccdeac711b2f013642d5a426f61dbb20b3e02
Egor Prokofjev <trueananas17@gmail.com>
2021-11-11 23:28:40 +0300

    final

--- trueananas17/hometask-2/get_middle_charachter.rb
+++ trueananas17/hometask-2/get_middle_charachter.rb
@@ -3,16 +3,16 @@
 # frozen_string_literal: true
 
 def get_middle(str)
-  half_a_len = str.length / 2
-  str.length.odd? ? str[half_a_len] : str[half_a_len - 1..half_a_len]
+  half_len = str.length / 2
+  str.length.odd? ? str[half_len] : str[half_len - 1..half_len]
 end
 
 def run_cli
   loop do
     puts 'Enter a string'
     str = gets.chomp.to_s
-    str.empty? ? (puts 'String can not be blank!') : (puts get_middle(str))
     break if str == 'exit!'
+    str.empty? ? (puts 'String can not be blank!') : (puts get_middle(str))
   end
 end
 
