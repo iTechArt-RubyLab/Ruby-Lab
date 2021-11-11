84c7c3a20870e220eb9265ad57d0fcf9057db6ec
Andrew <72628255+PivovarAndrew@users.noreply.github.com>
2021-11-11 03:45:26 +0300

    Update get_middle_character.rb

--- PivovarAndrew/hometask-2/get_middle_character.rb
+++ PivovarAndrew/hometask-2/get_middle_character.rb
@@ -5,7 +5,11 @@ def get_middle(str)
   middle_symbol_point = str.length / 2 - 1
   middle_symbol = str[middle_symbol_point]
   middle_next_symbol = str[middle_symbol_point + 1]
-  str.length == 1 ? str : (str.length.even? ? middle_symbol + middle_next_symbol : middle_next_symbol)
+  if str.length == 1
+    str
+  else
+    str.length.even? ? middle_symbol + middle_next_symbol : middle_next_symbol
+  end
 end
 
 def run_cli
