22028afc1d25359a57883a0794ce51b1d3dc9e32
vgoylo <y_draiv.by@mail.ru>
2021-11-12 00:59:40 +0300

    Add methods exist?

--- vgoylo/hometask-3/rainfall.rb
+++ vgoylo/hometask-3/rainfall.rb
@@ -14,7 +14,7 @@ data =
   'Lima:Jan 1.2,Feb 0.9,Mar 0.7,Apr 0.4,May 0.6,Jun 1.8,Jul 4.4,Aug 3.1,Sep 3.3,Oct 1.7,Nov 0.5,Dec 0.7'
 data1 =
   'Rome:Jan 90.2,Feb 73.2,Mar 80.3,Apr 55.7,May 53.0,Jun 36.4,Jul 17.5,Aug 27.5,Sep 60.9,Oct 147.7,Nov 121.0,Dec 97.9' + "\n" \
-  'London:Jan 58.0,Feb 38.9,Mar 49.9,Apr 42.2,May 67.3,Jun 52.1,Jul 59.5,Aug 77.2,Sep 55.4,Oct 62.0,Nov 69.0,Dec 52.9' + "\n" \
+  #'London:Jan 58.0,Feb 38.9,Mar 49.9,Apr 42.2,May 67.3,Jun 52.1,Jul 59.5,Aug 77.2,Sep 55.4,Oct 62.0,Nov 69.0,Dec 52.9' + "\n" \
   'Paris:Jan 182.3,Feb 120.6,Mar 188.1,Apr 204.9,May 323.1,Jun 350.5,Jul 336.8,Aug 192.9,Sep 66.3,Oct 63.3,Nov 83.2,Dec 154.7' + "\n" \
   'NY:Jan 128.7,Feb 121.8,Mar 151.9,Apr 93.5,May 98.8,Jun 93.6,Jul 142.2,Aug 131.8,Sep 92.0,Oct 82.3,Nov 107.8,Dec 94.2' + "\n" \
   'Vancouver:Jan 155.7,Feb 121.4,Mar 132.3,Apr 69.2,May 85.8,Jun 47.1,Jul 31.3,Aug 37.0,Sep 69.6,Oct 116.3,Nov 154.6,Dec 171.5' + "\n" \
@@ -32,6 +32,7 @@ def numbers(town, strng)
   strng.split("\n").select { |town_strng| town_strng.include?(town) }.first.scan(/\d+\.\d+/).map(&:to_f)
 end    
 
+
 def average(year)
   year.sum / 12
 end
@@ -40,17 +41,23 @@ def validate_town?(town)
   !$towns.include?(town)
 end
 
+def exist?(town, strng)
+  strng.split("\n").select { |town_strng| town_strng.include?(town) }.empty?
+end
+
 def mean(town, strng)
   return -1 if validate_town?(town)
+  return -1 if exist?(town, strng)
 
   average(numbers(town, strng))
 end
 
 
-puts mean('111', data1)
+puts mean('London', data1)
 
 def variance(town, strng)
   return -1 if validate_town?(town)
+  return -1 if exist?(town, strng)
 
   year = 
     numbers(town, strng).map do |element|
