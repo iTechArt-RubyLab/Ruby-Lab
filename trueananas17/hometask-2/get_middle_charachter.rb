81d6a0cf64e93d720026a02ad01c179dc218890c
Egor Prokofjev <trueananas17@gmail.com>
2021-11-11 20:06:10 +0300

    Update get_middle_charachter.rb

--- trueananas17/hometask-2/get_middle_charachter.rb
+++ trueananas17/hometask-2/get_middle_charachter.rb
@@ -1,28 +1,27 @@
 #!/usr/bin/env ruby
 
-def get_middle(s)
+def get_middle(str)
   if s.length % 2 == 0
-    mid = s[s.length/2-1]
-    mid += s[s.length/2]
+    mid = str[str.length/2-1]
+    mid += str[str.length/2]
     return mid
   elsif s.length % 2 != 0
-    mid = s[s.length/2]
+    mid = str[str.length / 2]
     return mid
   end
 end
 
-
-def run_cli()
+def run_cli
   while true
     puts "Enter a string"
-    s = gets.chomp.to_s
-    if s.empty? == true
+    str = gets.chomp.to_s
+    if str.empty? == true
       puts "String can not be blank!"
-    elsif s == "exit!"
+    elsif str == "exit!"
       break
-    else puts get_middle(s)
+    else puts get_middle(str)
     end
   end
 end
 
-run_cli()
+run_cli
