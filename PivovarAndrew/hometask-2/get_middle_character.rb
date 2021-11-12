fd696da78ff53504f16e7b66e9cfcd48975073e8
Andrew <72628255+PivovarAndrew@users.noreply.github.com>
2021-11-13 01:37:59 +0300

    Update get_middle_character.rb

--- PivovarAndrew/hometask-2/get_middle_character.rb
+++ PivovarAndrew/hometask-2/get_middle_character.rb
@@ -1,24 +1,23 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-EXIT = 'exit!'
-ERROR_MESSAGE = 'String can not be blank!'
+STOP_WORD = "exit!"
+ERROR_MESSAGE = "String can not be blank!"
 
 def get_middle(str)
-  middle_symbol_point = str.length / 2 - 1
+  middle_symbol_point = (str.length / 2).cell - 1
   str.length.even? ? str[middle_symbol_point..middle_symbol_point + 1] : str[middle_symbol_point]
 end
 
 def run_cli
   loop do
-    print 'Enter string: '
+    print "Enter string: "
     str = gets.chomp
-    if str == EXIT
+    if str == STOP_WORD
       exit!
     elsif str.empty?
       puts ERROR_MESSAGE
-    else puts "Middle character(s): #{get_middle(str)}"
-    end
+    else puts "Middle character(s): #{get_middle(str)}"     end
   end
 end
 
