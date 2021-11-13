086f7ba2e4ef9a7aafaa1f8cb138536e34d534d2
Andrew <72628255+PivovarAndrew@users.noreply.github.com>
2021-11-13 17:02:21 +0300

    Update rainfall.rb

--- PivovarAndrew/hometask-3/rainfall.rb
+++ PivovarAndrew/hometask-3/rainfall.rb
@@ -4,7 +4,7 @@
 STOP_WORD = "exit!"
 ERROR_MESSAGE = "String can not be blank!"
 
-data =
+EXAMPLE_DATA =
   "Rome:Jan 81.2,Feb 63.2,Mar 70.3,Apr 55.7,May 53.0,Jun 36.4,Jul 17.5,Aug 27.5,Sep 60.9,Oct 117.7,Nov 111.0,Dec 97.9" + "\n" +
   "London:Jan 48.0,Feb 38.9,Mar 39.9,Apr 42.2,May 47.3,Jun 52.1,Jul 59.5,Aug 57.2,Sep 55.4,Oct 62.0,Nov 59.0,Dec 52.9" + "\n" +
   "Paris:Jan 182.3,Feb 120.6,Mar 158.1,Apr 204.9,May 323.1,Jun 300.5,Jul 236.8,Aug 192.9,Sep 66.3,Oct 63.3,Nov 83.2,Dec 154.7" + "\n" +
@@ -16,8 +16,10 @@ data =
   "Beijing:Jan 3.9,Feb 4.7,Mar 8.2,Apr 18.4,May 33.0,Jun 78.1,Jul 224.3,Aug 170.0,Sep 58.4,Oct 18.0,Nov 9.3,Dec 2.7" + "\n" +
   "Lima:Jan 1.2,Feb 0.9,Mar 0.7,Apr 0.4,May 0.6,Jun 1.8,Jul 4.4,Aug 3.1,Sep 3.3,Oct 1.7,Nov 0.5,Dec 0.7"
 
-towns = ["Rome", "London", "Paris", "NY", "Vancouver", "Sydney", "Bangkok", "Tokyo",
+TOWNS = ["Rome", "London", "Paris", "NY", "Vancouver", "Sydney", "Bangkok", "Tokyo",
          "Beijing", "Lima", "Montevideo", "Caracas", "Madrid", "Berlin"]
+
+MONTHES = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
 class RainfallController
   attr_reader :town, :data
@@ -32,11 +34,11 @@ class RainfallController
   end
 
   def is_data_contain_town?
-    str.match(/^#{town}/)
+    data.match(/^#{town}/)
   end
 
   def get_rainfalls
-    parse_data(data)[town].scan(/\d+.\d+/).map(&:to_f)
+    parse_data[town].scan(/\d+.\d+/).map(&:to_f)
   end
 
   private
@@ -51,7 +53,7 @@ def mean(rainfall)
 end
 
 def variance(rainfall)
-  rainfall.is_data_contain_town? ? rainfall.get_rainfalls.map { |element| (element - mean(rain)) ** 2 }.sum / get_rainfalls.length : -1
+  rainfall.is_data_contain_town? ? rainfall.get_rainfalls.map { |element| (element - mean(rainfall)) ** 2 }.sum / rainfall.get_rainfalls.length : -1
 end
 
 def run_cli
@@ -63,7 +65,7 @@ def run_cli
     elsif city_name.empty?
       puts ERROR_MESSAGE
     else
-      rainfall_controller = RainfallController.new(city_name, data)
+      rainfall_controller = RainfallController.new(city_name, EXAMPLE_DATA)
       puts "Rainfall mean: #{mean(rainfall_controller)}"
       puts "Rainfall varince: #{variance(rainfall_controller)}"
     end
