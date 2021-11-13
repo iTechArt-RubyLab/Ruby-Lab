35646bf708eb0c437d03bb5ab73611428f727835
Alexey Kren <krien2000@mail,ru>
2021-11-13 22:42:15 +0300

    Deleted unnecessary spacing

--- Alexey-Kren/hometask-2/get_middle_character.rb
+++ Alexey-Kren/hometask-2/get_middle_character.rb
@@ -15,13 +15,11 @@ def run_cli
   loop do
     puts INPUT_PROMPT
     str = gets.chomp
-
     break if str == EXIT_STRING
-
     if str.empty?
       puts ERROR_EMPTY
     else
-      puts  "#{MESS_RESULT} #{get_middle(str)}"
+      puts "#{MESS_RESULT} #{get_middle(str)}"
     end
   end
 end
