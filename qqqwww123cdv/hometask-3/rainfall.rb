67831f62e5617beba1ac3475e5bcf454bf0afb2d
qqqwww123cdv <silitski2013@mail.ru>
2021-11-14 15:27:03 +0300

    Silitski/hometask3

---
+++ qqqwww123cdv/hometask-3/rainfall.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+def data_get
+  file = File.new('data.txt')
+  file.read
+end
+
+def weather_data(town, data)
+  data.split("\n").each do |i|
+    if i.include? town
+      str = i.scan(/\d*\.\d/).map(&:to_f)
+      return str
+    end
+  end
+end
+
+def mean(town, data)
+  return -1.0 unless data.include? town
+
+  weather_data(town, data).sum / 12
+end
+
+def variance(town, data)
+  return -1.0 unless data.include? town
+
+  weather_data(town, data).map { |i| (mean(town, data) - i)**2 }.sum / 12
+end
+
+def show_result(town)
+  puts "Rainfall mean: #{mean(town, data_get)}"
+  puts "Rainfall variance: #{variance(town, data_get)}"
+end
+
+def run_cli
+  loop do
+    puts 'Enter city name: '
+    str = gets.chomp
+    break if str == 'exit!'
+
+    if str.empty?
+      puts 'Error, line is empty!'
+    else
+      show_result(str)
+    end
+  end
+end
+run_cli
