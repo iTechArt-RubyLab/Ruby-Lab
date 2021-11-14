4f6265509a4cb7cc4e1965507ed8ef7b6d6798bd
Liza <skripchenkoveta@gmail.com>
2021-11-15 00:31:45 +0300

    refactor code

--- skripchenkoveta/hometask-3/rainfall.rb
+++ skripchenkoveta/hometask-3/rainfall.rb
@@ -3,49 +3,49 @@
 ENTER_MESSAGE = 'Enter city name: '
 ERROR_MESSAGE = 'City name can not be blank!'
 EXIT_COMMAND = 'exit!'
-MONTHS_YEAR = 12
+MONTHS_IN_YEAR = 12
 
 def rainfall_string
   file = File.new('data.txt', 'r:UTF-8')
   file.read
 end
 
-def town_search(town_rainfall, string_rainfall)
-  string_rainfall = string_rainfall.split("\n")
-  string_rainfall.find { |value| value.include? town_rainfall }
+def town_search(town, rainfall_string)
+  rainfall_string = rainfall_string.split("\n")
+  rainfall_string.find { |value| value.include? town }
 end
 
-def extract_numbers(string_rainfall)
-  string_rainfall.scan(/\d*\.\d/).map(&:to_f)
+def extract_numbers(rainfall_string)
+  rainfall_string.scan(/\d*\.\d/).map(&:to_f)
 end
 
-def calculate_middle_rainfall(town_rainfall, string_rainfall)
-  return -1.0 unless string_rainfall.include? town_rainfall
+def calculate_middle_rainfall(town, rainfall_string)
+  return -1.0 unless rainfall_string.include? town
 
-  average_rainfall = 0.0
-  string_rainfall = town_searching(town_rainfall, string_rainfall)
-  values_rainfall = extract_numbers(string_rainfall)
-  values_rainfall.each do |value|
-    average_rainfall += value
+  rainfall_average= 0.0
+  rainfall_string = town_search(town, rainfall_string)
+  rainfall_values = extract_numbers(rainfall_string)
+  rainfall_values.each do |value|
+    rainfall_average+= value
   end
-  average_rainfall / MONTHS_YEAR
+  rainfall_average/ MONTHS_IN_YEAR
 end
 
-def calculate_variance(town_rainfall, string_rainfall)
-  return -1.0 unless string_rainfall.include? town_rainfall
+def calculate_variance(town, rainfall_string)
+  return -1.0 unless rainfall_string.include? town
 
-  variance_rainfall = 0.0
-  string_rainfall = town_searching(town_rainfall, string_rainfall)
-  values_rainfall = extract_numbers(string_rainfall)
-  values_rainfall.each do |value|
-    variance_rainfall += (calculate_middle_rainfall(town_rainfall, string_rainfall) - value)**2
+  rainfall_variance = 0.0
+  rainfall_string = town_search(town, rainfall_string)
+  rainfall_values = extract_numbers(rainfall_string)
+  rainfall_values.each do |value|
+    rainfall_variance += (calculate_middle_rainfall(town, rainfall_string) - value)**2
   end
-  variance_rainfall / MONTHS_YEAR
+  rainfall_variance / MONTHS_IN_YEAR
 end
 
-def show_result(town_rainfall)
-  puts "Rainfall mean: #{calculate_middle_rainfall(town_rainfall, rainfall_string)}"
-  puts "Rainfall variance: #{calculate_variance(town_rainfall, rainfall_string)}"
+def show_result(town)
+  puts "Rainfall mean: #{calculate_middle_rainfall(town, rainfall_string)}"
+  puts "Rainfall variance: #{calculate_variance(town, rainfall_string)}"
 end
 
 def run_cli
