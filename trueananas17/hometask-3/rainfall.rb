977848890f95d83d198f364b7f1ae9a7f06eed8e
Egor Prokofjev <trueananas17@gmail.com>
2021-11-15 23:19:53 +0300

    Rubocop violations autocorrecion

--- trueananas17/hometask-3/rainfall.rb
+++ trueananas17/hometask-3/rainfall.rb
@@ -2,7 +2,7 @@
 
 # frozen_string_literal: true
 
-data = File.new("data.txt", "r:UTF-8").read
+data = File.new('data.txt', 'r:UTF-8').read
 
 def run_cli(data)
   loop do
@@ -10,7 +10,7 @@ def run_cli(data)
     town = gets.chomp.to_s
     break if town == 'exit!'
 
-    town.empty? ? (puts 'City name can not be blank!') : (output(parsed_town_data(town, data)))
+    town.empty? ? (puts 'City name can not be blank!') : output(parsed_town_data(town, data))
   end
 end
 
@@ -21,24 +21,24 @@ def output(town_data)
   puts variance(town_data)
 end
 
-  def mean(town_data)
-    town_data.sum / 12
-  end
+def mean(town_data)
+  town_data.sum / 12
+end
 
-  def parsed_town_data(town, data)
-    town_row(town, data).scan(/\d+[,.]\d+/).map!(&:to_f)
-  end
+def parsed_town_data(town, data)
+  town_row(town, data).scan(/\d+[,.]\d+/).map!(&:to_f)
+end
 
-  def town_row(town, data)
-    data.split("\n").find {|row| row.start_with?(town)}
-  end
+def town_row(town, data)
+  data.split("\n").find { |row| row.start_with?(town) }
+end
 
-  def variance(town_data)
-    sum = 0
-    town_data.each do |data|
-      sum += (data-mean(town_data))**2
-    end
-    sum
+def variance(town_data)
+  sum = 0
+  town_data.each do |data|
+    sum += (data - mean(town_data))**2
   end
+  sum
+end
 
 run_cli(data)
