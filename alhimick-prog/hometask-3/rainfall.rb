66dbb2fd7b2ef5ef70cf4df6324734be390a650d
alhimick-prog <alhimick96@yahoo.com>
2021-11-15 19:57:23 +0300

    from_substring_to_pulled

--- alhimick-prog/hometask-3/rainfall.rb
+++ alhimick-prog/hometask-3/rainfall.rb
@@ -4,7 +4,7 @@
 INPUT_PROMPT = 'Enter city name:'
 ERROR_BLANK_STRING = 'City name can not be blank!'
 ERROR_FILE_READ_FAIL = 'Error: data file not found'
-ERROR_MONTH_NUMBER = 'Error: something went wrong when counting months.'
+MESSAGE_INCORRECT_MONTH_NUMBER = 'Warning: the number of months is not equal to 12.'
 MEAN = 'Rainfall mean:'
 VARIANCE = 'Rainfall variance:'
 EXIT_STRING = 'exit!'
@@ -25,52 +25,29 @@ def file_reader(filename)
   end
 end
 
-# Looking for the specified city, determining its position in the line.
-# Since the city name is always at the beginning of the line, we check
-# that the previous character was a line feed or the beginning of a file.
-# If the city is not found - return false.
 def town_pos(town, strng)
-  pos = /(^|\s)#{town}:/ =~ strng
-  case pos
-  when /^#{town}:/ =~ strng
-    pos
-  when /\s#{town}:/ =~ strng
-    pos + 1
-  else
-    false
-  end
+  pos = /\s#{town}:/ =~ strng
+  pos ? pos + 1 : /^#{town}:/ =~ strng
 end
 
-# Form a substring with values belonging to the city.
-# Substring starts after city name and colon
-# and ends at newline or end of file.
-def form_substring(pos, strng)
-  substr = ''
-  loop do
-    substr += strng[pos]
-    pos += 1
-    break if strng[pos] == "\n" || pos == strng.size
-  end
-  substr
+def ending_search_point(start_search_pos, strng)
+  point = /\n/ =~ strng[start_search_pos..strng.size]
+  point ? (point + start_search_pos) : (strng.size - 1)
 end
 
-# Form an array of numbers from substring with town,
-# if the town was found.
 def form_array(town, strng)
-  if town_pos(town, strng)
-    start_search_pos = town_pos(town, strng) + town.size + 1
-    substr = form_substring(start_search_pos, strng)
-    substr.scan(/\d+\.*\d*/).map(&:to_f)
-  else
-    false
-  end
+  return nil unless town_pos(town, strng)
+
+  start_search_pos = town_pos(town, strng) + town.size + 1
+  ending_search_pos = ending_search_point(start_search_pos, strng)
+  strng[start_search_pos..ending_search_pos].scan(/\d+\.*\d*/).map(&:to_f)
 end
 
 def mean(town, strng)
   array = form_array(town, strng)
   return TOWN_ERROR unless array
 
-  puts ERROR_MONTH_NUMBER if array.size != MONTH_NUMBER
+  puts MESSAGE_INCORRECT_MONTH_NUMBER if array.size != MONTH_NUMBER
   array.sum / array.size
 end
 
@@ -82,7 +59,6 @@ def variance(town, strng)
   array.inject(0) { |memo, n| memo + (n - mean_val)**2 } / array.size
 end
 
-# Analyzing user input
 def inp_selection(town, strng)
   case town
   when EXIT_STRING
