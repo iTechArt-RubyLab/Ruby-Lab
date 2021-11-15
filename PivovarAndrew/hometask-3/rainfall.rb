9e8424e518c4f88ea41caf88af8320795d164235
Andrew <72628255+PivovarAndrew@users.noreply.github.com>
2021-11-15 21:20:07 +0300

    Freeze mutable objects assigned to constants

--- PivovarAndrew/hometask-3/rainfall.rb
+++ PivovarAndrew/hometask-3/rainfall.rb
@@ -1,27 +1,32 @@
 # !/usr/bin/env ruby
 # frozen_string_literal: false
 
-STOP_WORD = 'exit!'
+STOP_WORD = "exit!"
 STOP_WORD.freeze
-ERROR_MESSAGE = 'String can not be blank!'
+ERROR_MESSAGE = "String can not be blank!"
 ERROR_MESSAGE.freeze
 
 # Controller for the implementation of operations related to rainfalls
 class RainfallController
   TOWNS = %w[Rome London Paris NY Vancouver Sydney Bangkok
              Tokyo Beijing Lima Montevideo Caracas Madrid Berlin]
-
+  TOWNS.freeze
   MONTHES = %w[Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec]
+  MONTHES.freeze
 
   MIN_RAINFALLS_COUNT = 0.1
+  MIN_RAINFALLS_COUNT.freeze
   MAX_RAINFALLS_COUNT = 300.0
+  MAX_RAINFALLS_COUNT.freeze
   RAINFALL_ACCURACY = 1
-  DATA_FILE_NAME = 'data.txt'
+  RAINFALL_ACCURACY.freeze
+  DATA_FILE_NAME = "data.txt"
+  DATA_FILE_NAME.freeze
 
   attr_reader :data
   attr_accessor :town
 
-  def initialize(town = +'', data = +'')
+  def initialize(town = +"", data = +"")
     @town = town
     @data = data
   end
@@ -29,13 +34,13 @@ class RainfallController
   def generate_data
     TOWNS.each do |town|
       @data << "#{town}:"
-      @data << generate_rainfalls.compact.join(',')
+      @data << generate_rainfalls.compact.join(",")
       @data << "\n"
     end
   end
 
   def write_data_to_file
-    File.open(DATA_FILE_NAME, 'w') { |file| file.write(data) }
+    File.open(DATA_FILE_NAME, "w") { |file| file.write(data) }
   end
 
   def average
@@ -55,7 +60,7 @@ class RainfallController
   end
 
   def variance
-    data_contain_town? ? rainfalls.map { |rainfall| (rainfall - mean)**2 }.sum / rainfalls.length : -1
+    data_contain_town? ? rainfalls.map { |rainfall| (rainfall - mean) ** 2 }.sum / rainfalls.length : -1
   end
 
   private
@@ -81,7 +86,7 @@ def run_cli
   rainfall_controller.generate_data
   rainfall_controller.write_data_to_file
   loop do
-    print 'Enter city name: '
+    print "Enter city name: "
     city_name = gets.chomp
     if city_name == STOP_WORD
       exit!
