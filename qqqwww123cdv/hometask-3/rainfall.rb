fce8060f2592a9900c610aca1e668a725fabe1ff
qqqwww123cdv <silitski2013@mail.ru>
2021-11-15 22:17:04 +0300

    used find, renamed some variables, removed hardcode, got rid of a potential redundant operation

--- qqqwww123cdv/hometask-3/rainfall.rb
+++ qqqwww123cdv/hometask-3/rainfall.rb
@@ -7,25 +7,21 @@ def data
 end
 
 def weather_data(town, data)
-  data.split("\n").find do |value|
-    if value.include? town
-      monthly_rainfall = value.scan(/\d*\.\d/).map(&:to_f)
-      return monthly_rainfall
-    end
-  end
+  monthly_rainfall = data.split("\n").find { |data_row| data_row.match "#{town}:" }
+  return monthly_rainfall.scan(/\d*\.\d/).map(&:to_f) unless monthly_rainfall.nil?
 end
 
 def mean(town, data)
+  rainfall_values = weather_data(town, data)
   return -1.0 unless data.match "#{town}:"
 
-  average_rainfall = weather_data(town, data)
-  average_rainfall.sum / 12
+  rainfall_values.sum / rainfall_values.length
 end
 
 def variance(town, data)
+  rainfall_values = mean(town, data)
   return -1.0 if mean(town, data) == -1
 
-  rainfall_values = mean(town, data)
   weather_data(town, data).map { |data_row| (rainfall_values - data_row)**2 }.sum / 12
 end
 
