24ea99e5e6613154704cfca26c6ba57d0861528c
matveibos <matveibos@mail.ru>
2021-11-16 19:00:39 +0300

    adding cool changes

--- Matveibos/hometask-3/rainfall_stat.rb
+++ Matveibos/hometask-3/rainfall_stat.rb
@@ -9,14 +9,15 @@ class RainfallStat
     @town = town
     @data = data
     @processed_data = process_data
+    rainfall_data_for(town)
   end
 
   def mean
-    rainfall_data_for(town).sum / rainfall_data_for(town).size
+    @rainfall_data_for.sum / @rainfall_data_for.size
   end
 
   def variance
-    rainfall_data_for(town).inject(0) { |var, n| var + (n - mean)**2 / rainfall_data_for(town).size }
+    @rainfall_data_for.inject(0) { |var, n| var + (n - mean)**2 / @rainfall_data_for.size }
   end
 
   private
@@ -24,16 +25,13 @@ class RainfallStat
   attr_reader :town, :data
 
   def process_data
-    hash = {}
-    data.each_line do |str|
-      city_name = str.split(SEPARATOR).first
-      precipitation = str.scan(CHOOSING_NUMBERS).map(&:to_f)
-      hash[city_name] = precipitation
+    data.each_line.each_with_object({}) do |line, processed_data|
+      processed_data[line.split(SEPARATOR).first] = line.scan(CHOOSING_NUMBERS).map(&:to_f)
     end
-    hash
   end
 
   def rainfall_data_for(town)
-    @processed_data[town]
+    @rainfall_data_for ||= @processed_data[town]
+    puts @rainfall_data_for.class
   end
 end
