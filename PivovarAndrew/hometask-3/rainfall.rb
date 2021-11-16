c52166cfac5283fa491d1500531a09427a3f7179
Andrew <72628255+PivovarAndrew@users.noreply.github.com>
2021-11-16 16:55:34 +0300

    Main rainfall class division on friend classes

--- PivovarAndrew/hometask-3/rainfall.rb
+++ PivovarAndrew/hometask-3/rainfall.rb
@@ -1,38 +1,54 @@
 # !/usr/bin/env ruby
 # frozen_string_literal: false
 
-STOP_WORD = 'exit!'
-ERROR_MESSAGE = 'String can not be blank!'
+STOP_WORD = 'exit!'.freeze
+ERROR_MESSAGE = 'String can not be blank!'.freeze
+DATA_FILE_NAME = 'data.txt'.freeze
+ENTER_MESSAGE = 'Enter city name: '.freeze
 
-# Controller for the implementation of operations related to rainfalls
-class RainfallController
-  TOWNS = %w[Rome London Paris NY Vancouver Sydney Bangkok
-             Tokyo Beijing Lima Montevideo Caracas Madrid Berlin]
-  MONTHES = %w[Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec]
-
-  MIN_RAINFALLS_COUNT = 0.1
-  MAX_RAINFALLS_COUNT = 300.0
-  RAINFALL_ACCURACY = 1
-  DATA_FILE_NAME = 'data.txt'
-
-  attr_reader :data
-  attr_accessor :town
-
-  def initialize(town = +'', data = +'')
-    @town = town
-    @data = data
+# Class for the implementation of operations related to files
+class FileService
+  def write_data_to_file(data, file_name)
+    File.open(file_name, 'w') { |file| file.write(data) }
   end
+end
+
+# Class for the implementation of operations related to rainfalls
+class GenerateRainfallData
+  TOWNS = %w[Rome London Paris NY Vancouver Sydney Bangkok
+             Tokyo Beijing Lima Montevideo Caracas Madrid Berlin].freeze
+  MONTHES = %w[Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec].freeze
+
+  MIN_RAINFALLS_COUNT = 0.1.freeze
+  MAX_RAINFALLS_COUNT = 300.0.freeze
+  RAINFALL_ACCURACY = 1.freeze
 
   def generate_data
-    TOWNS.each do |town|
-      @data << "#{town}:"
-      @data << generate_rainfalls.compact.join(',')
-      @data << "\n"
+    TOWNS.map.each_with_object(String.new) do |town, memo|
+      memo << "#{town}:#{generate_rainfalls.join(',')}"
+      memo << "\n"
     end
   end
 
-  def write_data_to_file
-    File.open(DATA_FILE_NAME, 'w') { |file| file.write(data) }
+  private
+
+  def generate_rainfalls
+    min_generated_value = rand(MIN_RAINFALLS_COUNT..MAX_RAINFALLS_COUNT)
+    max_generated_value = min_generated_value * 1.5
+    MONTHES.map do |month|
+      "#{month} #{rand(min_generated_value..max_generated_value).round(RAINFALL_ACCURACY)}"
+    end
+  end
+end
+
+# Class to describe rainfall characteristics
+class Rainfall
+  attr_reader :data
+  attr_accessor :town
+
+  def initialize(town = +'', data = GenerateRainfallData.new.generate_data)
+    @town = town
+    @data = data
   end
 
   def average
@@ -60,34 +76,22 @@ class RainfallController
   def parse_data
     data.split("\n").map { |data_str| data_str.split(':') }.to_h
   end
-
-  def generate_rainfalls
-    random = Random.new
-    min_generated_value = random.rand(MIN_RAINFALLS_COUNT..MAX_RAINFALLS_COUNT)
-    max_generated_value = min_generated_value * 1.5
-    rainfalls_info = []
-    MONTHES.each do |month|
-      rainfalls_info << "#{month} #{random.rand(min_generated_value..max_generated_value).round(RAINFALL_ACCURACY)}"
-    end
-    rainfalls_info << nil
-  end
 end
 
 def run_cli
-  rainfall_controller = RainfallController.new
-  rainfall_controller.generate_data
-  rainfall_controller.write_data_to_file
+  rainfall = Rainfall.new
+  FileService.new.write_data_to_file(rainfall.data, DATA_FILE_NAME)
   loop do
-    print 'Enter city name: '
+    print ENTER_MESSAGE
     city_name = gets.chomp
     if city_name == STOP_WORD
       exit!
     elsif city_name.empty?
       puts ERROR_MESSAGE
     else
-      rainfall_controller.town = city_name
-      puts "Rainfall mean: #{rainfall_controller.mean}"
-      puts "Rainfall varince: #{rainfall_controller.variance}"
+      rainfall.town = city_name
+      puts "Rainfall mean: #{rainfall.mean}"
+      puts "Rainfall varince: #{rainfall.variance}"
     end
   end
 end
