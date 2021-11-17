b72b6c4db60026aa2729489424ba7a89550c0d7d
Liza <skripchenkoveta@gmail.com>
2021-11-17 13:51:31 +0300

    use rubocop

--- skripchenkoveta/hometask-4/my_array_rotate.rb
+++ skripchenkoveta/hometask-4/my_array_rotate.rb
@@ -1,15 +1,20 @@
+# frozen_string_literal: true
+
 class Array
-    def lab_rotate(value=1)
-      step = value % self.length
-      return self unless step !=self.length
-       self.last(self.length - step) + self.first(step)
-      end
-      def lab_rotate!(value=1)
-        step = value % self.length
-        return self unless step !=self.length
-        step.times do |i|
-          self.push(self.shift)
-        end
-        self
-      end
+  def lab_rotate(value = 1)
+    step = value % length
+    return self unless step != length
+
+    last(length - step) + first(step)
+  end
+
+  def lab_rotate!(value = 1)
+    step = value % length
+    return self unless step != length
+
+    step.times do |_i|
+      push(shift)
+    end
+    self
+  end
 end
