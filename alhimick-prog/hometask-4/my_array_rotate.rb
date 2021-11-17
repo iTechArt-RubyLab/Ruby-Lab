f76ad2b4b84749a6dfeed61d4da35703dc1b6404
alhimick-prog <alhimick96@yahoo.com>
2021-11-17 19:00:41 +0300

    realize_array_rotate

--- alhimick-prog/hometask-4/my_array_rotate.rb
+++ alhimick-prog/hometask-4/my_array_rotate.rb
@@ -4,46 +4,53 @@
 class Array
 
   def lab_rotate(n=1)
-    itself_rotate(self, n)
+    check_before_rotate(self, n)
   end
 
   def lab_rotate!(n=1)
-    new_order = itself_rotate(self, n)
-    puts "New order: #{new_order}"
-    i = 0
-    self.each_with_index do |i, v|
+    new_order = check_before_rotate(self, n)
+    self.each_index do |i|
       self[i] = new_order[i]
     end
-    #itself_rotate(self, n)
-    #self << 0
-    #self << 1
-    puts "#{self}"
     self
   end
 
   private
 
-  def itself_rotate(array, n)
+  def check_before_rotate(array, n)
     return [] if array.empty?
     return array if n.zero?
-    return array if array.size % n == 0
-    array = (1..10).to_a
-    #puts "Array rotate to #{n} move to #{array}"
-    array
+    return array if n >= array.size && array.size % n == 0
+
+    n = n % array.size if n > array.size
+    itself_rotate(array, n)
+  end
+
+  def itself_rotate(array, n)
+    new_order = []
+    start_point = n > 0 ? n : array.size + n
+    array[start_point...array.size].each { |n| new_order.push(n) }
+    array[0...start_point].each { |n| new_order.push(n) }
+    new_order
   end
 
 end
 
+
+=begin
 arr = [1, 2, 3, 4, 5, 6, 7]
-#arr = []
 puts "Start arr: #{arr}"
-puts "ID: #{arr.object_id}\n"
+puts "ID: #{arr.object_id}"
 puts "Rotate(): #{arr.lab_rotate}"
 puts "After: #{arr}"
-puts "ID: #{arr.object_id}\n"
+puts "ID: #{arr.object_id}"
 puts "Rotate(10): #{arr.lab_rotate(10)}"
 puts "After: #{arr}"
-puts "#{arr.object_id}\n"
+puts "#{arr.object_id}"
 puts "Rotate!(): #{arr.lab_rotate!}"
 puts "After: #{arr}"
-puts "#{arr.object_id}\n"
+puts "#{arr.object_id}"
+puts "Rotate!(-5): #{arr.lab_rotate!(-5)}"
+puts "After: #{arr}"
+puts "#{arr.object_id}"
+=end
