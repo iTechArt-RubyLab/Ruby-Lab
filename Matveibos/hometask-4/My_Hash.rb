a6d903a88592d24368662c74f8147cf8d7adc58e
matveibos <matveibos@mail.ru>
2021-11-18 15:57:33 +0300

    first task is ok, but second task is bad :(

---
+++ Matveibos/hometask-4/My_Hash.rb
@@ -0,0 +1,68 @@
+# frozen_string_literal: true
+
+# the module
+class MyHash
+  prepend Enumerable
+
+  def initialize
+    @arr = []
+    @indicator = []
+  end
+
+  def [](key)
+    find_in_array(key)[1]
+  end
+
+  def []=(key, value)
+    find_in_array(key)
+    @indicator == [] ? @arr.push([key, value]) : @indicator[1] = value
+  end
+
+  def find_in_array(key)
+    @indicator = @arr.find { |findkey, _value| findkey == key } || []
+  end
+
+  def clear
+    @arr.clear
+  end
+
+  def size
+    @arr.size
+  end
+
+  def each(&block)
+    @arr.each(&block)
+  end
+
+  def delete(key)
+    @arr.delete(find_in_array(key))
+  end
+end
+
+h = MyHash.new
+h['key1'] = 'value1'
+h['key1'] = 'value3'
+h['key13'] = 'value1'
+h['key13'] = 'value4'
+puts h['key13']
+h.each { |k, v| puts "#{k} => #{v}" }
+# h.inspect
+# h.arr.each {|k, v| print "{#{k} => #{v}}" }
+# h['key1'] = 'value2'
+# print "#{h.arr}\n"
+# h['key2'] = 'value2'
+# print "#{h.arr}\n"
+# puts h.size
+#
+# h.arr.each do |k,v|
+#   print "{#{k} => #{v}}"
+# end
+#
+#  h['key3'] = 'value3'
+# print "#{h.arr}\n"
+#
+# h.delete_key('key2')
+# print "#{h.arr}\n"
+# #
+# h.delete_key_and_value
+# print "#{h.arr}\n"
---
+++ Matveibos/hometask-4/My_Hash_benchmark.rb..rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'benchmark'
+require './My_Hash'
+
+puts '100'
+
+Benchmark.bm do |x|
+  hash = {}
+  my_hash = MyHash.new
+  puts '100'
+  x.report { 100.times { |el,al| hash[el] = al } }
+  x.report { 100.times { |el,al| my_hash[el] = al } }
+  hash.clear
+  my_hash.clear
+  puts '1000'
+  x.report { 1000.times { |el| hash[el] = el } }
+  x.report { 1000.times { |el| my_hash[el] = el } }
+  hash.clear
+  my_hash.clear
+  puts '10000'
+  x.report { 10_000.times { |el| hash[el] = el } }
+  x.report { 10_000.times { |el| my_hash[el] = el } }
+  hash.clear
+  my_hash.clear
+  puts 'read'
+  puts '100'
+  x.report { hash[100] }
+  x.report { my_hash[100] }
+  puts '1000'
+  x.report { hash[1000] }
+  x.report { my_hash[1000] }
+  puts '100000'
+  x.report { hash[100_000] }
+  x.report { my_hash[100_000] }
+end
