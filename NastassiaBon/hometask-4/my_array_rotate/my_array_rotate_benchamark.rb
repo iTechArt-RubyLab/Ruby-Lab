ee09c108b0adb7298322be8e192d628454c40f19
Nastya <Nastassia.Bandarenka@itechart-group.com>
2021-11-18 13:00:52 +0200

    Add Benchmark file

---
+++ NastassiaBon/hometask-4/my_array_rotate/my_array_rotate_benchamark.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'benchmark'
+require_relative 'my_array_rotate'
+
+Benchmark.bm do |x|
+  puts 'Lab_rotate!/rotate! Array size are 10 items'
+  x.report { (1..10).to_a.rotate!(4) }
+  x.report { (1..10).to_a.rotate!(7) }
+  x.report { (1..10).to_a.lab_rotate!(4) }
+  x.report { (1..10).to_a.lab_rotate!(7) }
+
+  puts 'Lab_rotate!/rotate! Array size are 1000 items'
+  x.report { (1..1000).to_a.rotate!(25) }
+  x.report { (1..1000).to_a.rotate!(99) }
+  x.report { (1..1000).to_a.lab_rotate!(25) }
+  x.report { (1..1000).to_a.lab_rotate!(99) }
+
+  puts 'Lab_rotate!/rotate Array size are 10 items'
+  x.report { (1..10).to_a.rotate(4) }
+  x.report { (1..10).to_a.rotate(7) }
+  x.report { (1..10).to_a.lab_rotate(4) }
+  x.report { (1..10).to_a.lab_rotate(7) }
+
+  puts 'Lab_rotate!/rotate! Array size are 1000 items'
+  x.report { (1..1000).to_a.rotate(10) }
+  x.report { (1..1000).to_a.rotate(99) }
+  x.report { (1..1000).to_a.lab_rotate(10) }
+  x.report { (1..1000).to_a.lab_rotate(99) }
+end
