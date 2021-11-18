3ea1f623e4fbfea6f62429c639f6b7fc72a5aed5
Gek <abasovwork@gmail.com>
2021-11-18 19:14:11 +0300

    Update my_array_rotate.rb

--- Gek62/hometask-4/my_array_rotate.rb
+++ Gek62/hometask-4/my_array_rotate.rb
@@ -4,7 +4,9 @@
 class Array
   def lab_rotate(displacement = 1)
     displacement = displacement % size
+    # rubocop:disable Style/SlicingWithRange
     self[displacement..-1] + self[0...displacement]
+    # rubocop:disable Style/SlicingWithRange
   end
 
   def lab_rotate!(displacement = 1)
