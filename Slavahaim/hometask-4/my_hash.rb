c85b61b2e29e1c84fe685bf9e484170404aa75e0
Slavahaim <slava.irizanov@gmail.com>
2021-11-18 20:57:54 +0300

    Add hometask 4.2

---
+++ Slavahaim/hometask-4/my_hash.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+# this class is extension for Hash
+class MyHash
+  include Enumerable
+  def initialize
+    @my_hash = []
+  end
+
+  def [](input_key)
+    pair(input_key)[1]
+  end
+
+  def []=(input_key, input_value)
+    if self[input_key]
+      @my_hash.find { |key, _value| key == input_key }[1] = input_value
+    else
+      @my_hash << [input_key, input_value]
+    end
+  end
+
+  def size
+    @my_hash.size
+  end
+
+  def delete(input_key)
+    @my_hash.delete(pair(input_key))
+  end
+
+  def delete_all
+    @my_hash.clear
+  end
+
+  def pair(input_key)
+    @my_hash.find { |key, _value| key == input_key } || []
+  end
+
+  def each(&block)
+    @my_hash.each(&block)
+  end
+end
