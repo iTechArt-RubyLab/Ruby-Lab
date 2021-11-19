e5c2f0d76db20897f7882a3214de5311ee3b1566
alhimick-prog <alhimick96@yahoo.com>
2021-11-20 00:52:42 +0300

    revork_myhash

--- alhimick-prog/hometask-4/my_hash.rb
+++ alhimick-prog/hometask-4/my_hash.rb
@@ -7,45 +7,125 @@ class MyHash
 
   KEY_POS = 0
   VALEU_POS = 1
+  START_SIZE = 13
+  LOAD_FACTOR = 0.5
+  START_POWER_OF_TWO = 16
+  START_ARRAY_OF_PRIME = [2, 3, 5, 7, 11, 13]
   KEY_IS_BUSY_ERROR = 'Key is busy!'
 
   def initialize
-    @values = []
+    @prime_numbers = START_ARRAY_OF_PRIME
+    @current_size = START_SIZE
+    @limiting_power_of_two = START_POWER_OF_TWO
+    @hash_array = Array.new(@current_size) { [] }
+    @size = 0
+    @temp_array = []
+    @position = nil
   end
 
   def [](key)
     value = nil
-    @values.each do |item|
+    @hash_array[define_pos(key)].each do |item|
       value = item[VALEU_POS] if item[KEY_POS].equal?(key)
     end
+    @position = nil
     value
   end
 
   def []=(key, value)
-    are_in_hash?(key) ? puts(KEY_IS_BUSY_ERROR) : set(key, value)
-  end
-
-  def are_in_hash?(key)
-    are = false
-    @values.each do |key_and_value|
-      are = true if key_and_value[KEY_POS].equal?(key)
+    if pos_in_bucket_if_present_in_hash(key)
+      @position = nil
+      return KEY_IS_BUSY_ERROR
     end
-    are
+
+    insertion(key, value)
+    @position = nil
+    @size += 1
+    increase_hash_array if @size > @current_size * 0.5
   end
 
-  def set(key, value)
-    @values.push([key, value])
+  def delete(key)
+    pos_in_bucket = pos_in_bucket_if_present_in_hash(key)
+    if pos_in_bucket
+      @hash_array[@position].delete_at(pos_in_bucket)
+      @size -= 1
+      @position = nil
+    end
   end
 
   def size
-    @values.size
-  end
-
-  def each(&block)
-    @values.each(&block)
+    @size
   end
 
   def clear
-    @values = []
+    @prime_numbers = START_ARRAY_OF_PRIME
+    @current_size = START_SIZE
+    @limiting_power_of_two = START_POWER_OF_TWO
+    @hash_array = Array.new(@current_size) { [] }
+    @size = 0
+    @temp_array = []
+    @position = nil
+  end
+
+  private
+
+  def insertion(key, value)
+    @hash_array[@position].push([key, value])
+  end
+
+  def each(&block)
+    @hash_array.each do |line_of_pair|
+      line_of_pair.each(&block)
+    end
+  end
+
+  def define_pos(key)
+    hash_value = key.hash
+    if hash_value.positive?
+      @position = hash_value % @current_size
+    else
+      @position = -hash_value % @current_size
+    end
+  end
+
+  def pos_in_bucket_if_present_in_hash(key)
+    define_pos(key)
+    @hash_array[@position].each_with_index do |item, index|
+      return index if item[KEY_POS].equal?(key)
+    end
+  end
+
+  def increase_hash_array
+    prime_numb_generator
+    array_of_pair
+    @current_size = @prime_numbers.last
+    @hash_array = Array.new(@current_size) {Array.new}
+    reinsert
+  end
+
+  def prime_numb_generator
+    (@limiting_power_of_two...(@limiting_power_of_two *= 2)).each do |tested_number|
+      is_prime = true
+      @prime_numbers.each do |divider|
+        is_prime = false if tested_number % divider == 0
+      end
+      @prime_numbers.push(tested_number) if is_prime
+    end
+  end
+
+  def array_of_pair
+    @hash_array.each do |line_of_pair|
+      line_of_pair.each do |pair|
+        @temp_array.push(pair) if pair
+      end
+    end
+  end
+
+  def reinsert
+    @size = 0
+    @temp_array.each do |pair|
+      self[pair[KEY_POS]] = pair[VALEU_POS]
+    end
+    @temp_array = []
   end
 end
