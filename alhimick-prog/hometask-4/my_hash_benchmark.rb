6466e58a45fd7c4798a199eec6af2c3c9b13a9f3
alhimick-prog <alhimick96@yahoo.com>
2021-11-21 19:23:10 +0300

    my_hash_benchmark_style_fix

--- alhimick-prog/hometask-4/my_hash_benchmark.rb
+++ alhimick-prog/hometask-4/my_hash_benchmark.rb
@@ -4,68 +4,59 @@
 require 'benchmark'
 require './my_hash'
 
-native_hash = {}
-my_hash = MyHash.new
-
-Benchmark.bm do |x|
-  puts 'Creation:'
-  x.report('native') { native_hash = {} }
-  x.report('custom') { my_hash = MyHash.new }
-end
-
 Benchmark.bm do |x|
   native_hash = {}
   my_hash = MyHash.new
   puts 'Insertion:'
   puts '<<<<<Size: 1000>>>>>'
   x.report('native') do
-    1000.times { |i| native_hash["#{i}"] = "it's #{i}" }
+    1000.times { |i| native_hash[i.to_s] = "it's #{i}" }
   end
   x.report('custom') do
-    1000.times { |i| my_hash["#{i}"] = "it's #{i}" }
+    1000.times { |i| my_hash[i.to_s] = "it's #{i}" }
   end
   puts '<<<<<Size: 1000 to 1_000_000>>>>>'
   x.report('native') do
-    1_000_000.times { |i| native_hash["#{i}"] = "it's #{i}" }
+    1_000_000.times { |i| native_hash[i.to_s] = "it's #{i}" }
   end
   x.report('custom') do
-    1_000_000.times { |i| my_hash["#{i}"] = "it's #{i}" }
+    1_000_000.times { |i| my_hash[i.to_s] = "it's #{i}" }
   end
 end
 
 Benchmark.bm do |x|
   native_hash = {}
   my_hash = MyHash.new
-  1_000_000.times { |i| native_hash["#{i}"] = "it's #{i}" }
-  1_000_000.times { |i| my_hash["#{i}"] = "it's #{i}" }
+  1_000_000.times { |i| native_hash[i.to_s] = "it's #{i}" }
+  1_000_000.times { |i| my_hash[i.to_s] = "it's #{i}" }
   puts 'Reading when 1_000_000 pairs:'
   puts '<<<<<Count: 10>>>>>'
   x.report('native') do
     10.times do
       i = rand(0..999_999)
-      native_hash["#{i}"]
+      native_hash[i.to_s]
     end
   end
   x.report('custom') do
     10.times do
       i = rand(0..999_999)
-      my_hash["#{i}"]
+      my_hash[i.to_s]
     end
   end
   puts '<<<<<Count: 1_000_000>>>>>'
   x.report('native') do
-    1_000_000.times { |i| native_hash["#{i}"] }
+    1_000_000.times { |i| native_hash[i.to_s] }
   end
   x.report('custom') do
-    1_000_000.times { |i| my_hash["#{i}"] }
+    1_000_000.times { |i| my_hash[i.to_s] }
   end
 end
 
 Benchmark.bm do |x|
   native_hash = {}
   my_hash = MyHash.new
-  1_000_000.times { |i| native_hash["#{i}"] = "it's #{i}" }
-  1_000_000.times { |i| my_hash["#{i}"] = "it's #{i}" }
+  1_000_000.times { |i| native_hash[i.to_s] = "it's #{i}" }
+  1_000_000.times { |i| my_hash[i.to_s] = "it's #{i}" }
   puts 'Counting size when 1_000_000 pairs:'
   x.report('native') { native_hash.size }
   x.report('custom') { my_hash.size }
