c702a1510db96064cda49eb635327542ddba08d0
anton <tehanovanton@gmail.com>
2021-11-21 22:27:04 +0300

    MyHash refactoring

--- TehanovAnton/hometask-4/my_hash.rb
+++ TehanovAnton/hometask-4/my_hash.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-# this class is my realisation of Hash
+# This is my Hash realisation
 class MyHash
   include Enumerable
 
-  LOAD_FACTOR = 0.7
+  LOAD_FACTOR = 0.9
   DEFAULT_SIZE = 10
 
   def initialize
@@ -12,18 +12,17 @@ class MyHash
   end
 
   def [](key)
-    pair = pair(key)
-
-    pair&.last
+    key_value = pair(key)
+    key_value&.last
   end
 
   def []=(key, value)
     resize! if resize?
 
-    slot = slot(key)
-    pair = pair(key, slot: slot)
+    current_slot = slot(key)
+    key_value = pair(key, slot: current_slot)
 
-    pair.nil? ? add_pair(key, value, slot: slot) : pair[1] = value
+    key_value.nil? ? add_pair(key, value, slot: current_slot) : key_value[1] = value
   end
 
   def delete(key)
@@ -38,6 +37,10 @@ class MyHash
     @pair_count
   end
 
+  def size
+    @pair_count
+  end
+
   def each(&block)
     @slots.each(&block)
   end
@@ -46,8 +49,8 @@ class MyHash
 
   attr_reader :pair_count, :slots_count, :slots
 
-  def hash_key(key, hash_size: slots_count)
-    key.hash % hash_size
+  def hash_key(key)
+    key.hash % slots_count
   end
 
   def resize?
@@ -58,8 +61,8 @@ class MyHash
     old_slots = slots
     initialize_hash!(slots_count: slots_count * 2)
 
-    old_slots.each do |slot|
-      slot.each do |key, value|
+    old_slots.each do |current_slot|
+      current_slot.each do |key, value|
         self[key] = value
       end
     end
@@ -69,11 +72,13 @@ class MyHash
     slots[hash_key(key)]
   end
 
-  def pair(key, slot: slot(key))
+  def pair(key, slot: nil)
+    slot ||= slot(key)
     slot.find { |k, _| k == key }
   end
 
-  def add_pair(key, value, slot: slot(key))
+  def add_pair(key, value, slot: nil)
+    slot ||= slot(key)
     slot.push([key, value])
     @pair_count += 1
   end
