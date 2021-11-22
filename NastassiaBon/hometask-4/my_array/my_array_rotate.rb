d97c1709327eff2cca931391990612a7478191ac
Nastya <Nastassia.Bandarenka@itechart-group.com>
2021-11-22 22:31:59 +0200

    Add bencmark files

--- NastassiaBon/hometask-4/my_array/my_array_rotate.rb
+++ NastassiaBon/hometask-4/my_array/my_array_rotate.rb
@@ -4,25 +4,25 @@
 
 # Array with my rotate Task 4.1
 class Array
-    def lab_rotate(value = 1)
-      my_shift = value % size
-  
-      if my_shift.zero?
-        self
-      else
-        last(size - my_shift) + first(my_shift)
-      end
+  def lab_rotate(value = 1)
+    my_shift = value % size
+
+    if my_shift.zero?
+      self
+    else
+      last(size - my_shift) + first(my_shift)
     end
-  
-    def lab_rotate!(value = 1)
-      my_shift = value % size
-  
-      if my_shift.zero?
-        self
-      else
-        my_shift.times do |_step|
-          push(shift)
-        end
+  end
+
+  def lab_rotate!(value = 1)
+    my_shift = value % size
+
+    if my_shift.zero?
+      self
+    else
+      my_shift.times do |_step|
+        push(shift)
       end
     end
   end
+end
