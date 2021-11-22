bf15e2ecc770760dd0c31bc58ca9f93b4c07ee10
Romuashi <romadyn@yandex.ru>
2021-11-22 19:37:28 +0300

    Update my_hash

--- Romuashi/hometask-4/my_hash.rb
+++ Romuashi/hometask-4/my_hash.rb
@@ -4,39 +4,73 @@
 class MyHash
   include Enumerable
 
+  LOAD_FACTOR = 0.7
+
   def initialize
-    @dictionary = []
+    dictionary
   end
 
   def [](key)
-    pair(key)[1]
+    arr_find(key_hash(key), key)&.last
   end
 
   def []=(key, value)
-    if self[key]
-      value.find { |k, _v| k == key }[1] = value
+    resize if count > size * LOAD_FACTOR
+    id = key_hash(key)
+    pair = arr_find(id, key)
+    if pair
+      pair[1] = value
     else
-      @dictionary.push([key, value])
+      push_pair(id, key, value)
     end
   end
 
   def delete(key)
-    @dictionary.delete(pair(key))
+    arr[key_hash(key)].delete_if { |k, _| k == key }
   end
 
-  def delete_all
-    @dictionary.clear
+  def clear
+    dictionary
   end
 
-  def size
-    @dictionary.size
-  end
-
-  def pair(key)
-    @dictionary.find { |k, _| k == key } || []
+  def length
+    count
   end
 
   def each(&block)
-    @dictionary.each(&block)
+    arr.each(&block)
+  end
+
+  private
+
+  attr_reader :count, :arr, :size
+
+  def arr_find(id, key)
+    arr[id].find { |k, _| k == key }
+  end
+
+  def key_hash(key)
+    key.hash % size
+  end
+
+  def push_pair(id, key, value)
+    arr[id].push([key, value])
+    @count += 1
+  end
+
+  def resize
+    old_cell = arr
+    dictionary(size: size * 4)
+    old_cell.each do |old_cel|
+      old_cel.each do |key, value|
+        self[key] = value
+      end
+    end
+  end
+
+  def dictionary(size: 10)
+    @size = size
+    @count = 0
+    @arr = Array.new(@size) { [] }
   end
 end
