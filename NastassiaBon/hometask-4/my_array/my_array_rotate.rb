cb55bcae5dcf678782a16ab968edce032de53a3f
Nastya <Nastassia.Bandarenka@itechart-group.com>
2021-11-22 22:27:42 +0200

    hometask-4, my array rotate/ my hash

---
+++ NastassiaBon/hometask-4/my_array/my_array_rotate.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+
+# frozen_string_literal: true
+
+# Array with my rotate Task 4.1
+class Array
+    def lab_rotate(value = 1)
+      my_shift = value % size
+  
+      if my_shift.zero?
+        self
+      else
+        last(size - my_shift) + first(my_shift)
+      end
+    end
+  
+    def lab_rotate!(value = 1)
+      my_shift = value % size
+  
+      if my_shift.zero?
+        self
+      else
+        my_shift.times do |_step|
+          push(shift)
+        end
+      end
+    end
+  end
---
+++ NastassiaBon/hometask-4/my_hash/my_hash.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/env ruby
+
+# frozen_string_literal: true
+
+# class My Hash task 4.2
+class MyHash
+  include Enumerable
+
+  def initialize
+    @keys = []
+  end
+
+  def [](key)
+    pair(key)[1]
+  end
+
+  def []=(key, value)
+    @keys.push([key, value])
+  end
+
+  def pair(key)
+    @keys.find { |key_value, _| key_value == key } || []
+  end
+
+  def delete(key)
+    @keys.delete(pair(key))
+  end
+
+  def clear
+    @keys.clear
+  end
+
+  def size
+    @keys.size
+  end
+
+  def each(&block)
+    @keys.each(&block)
+  end
+end
