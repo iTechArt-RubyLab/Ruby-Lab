824a162767b046e36730afb3ad2b1f74bb89ebde
Matveibos <71291376+Matveibos@users.noreply.github.com>
2021-11-22 22:56:28 +0300

    Matveibos hometask-5 (#127)
    * SQL tasks
    
    * SQL tasks revorked
    
    * fix bugs and spaces
    
    * fix 4.2
    
    * fix file names

---
+++ Matveibos/hometask-5/4.2.sql
@@ -0,0 +1,5 @@
+SELECT film.*
+FROM film
+INNER JOIN language 
+ON film.language_id = language.language_id
+WHERE language.name IN ('Italian', 'English', 'German')
---
+++ Matveibos/hometask-5/4.5.sql
@@ -0,0 +1,5 @@
+SELECT name, COUNT(language.language_id)
+FROM film 
+INNER JOIN language
+ON film.language_id = language.language_id
+GROUP BY name;
