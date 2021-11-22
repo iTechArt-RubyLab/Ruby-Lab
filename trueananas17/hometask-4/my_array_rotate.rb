38d09999b81b30c1373b8de5a2a19d7056a8f623
trueananas17 <trueananas17@gmail.com>
2021-11-22 18:21:22 +0300

    Create files

---
+++ trueananas17/hometask-4/my_array_rotate.rb
@@ -0,0 +1,20 @@
+# !/usr/bin/env ruby
+
+# frozen_string_literal: true
+
+#Custom Array methods
+class Array
+  def lab_rotate(shift = 1)
+    shift %= length
+    first_part = self[0..shift - 1]
+    second_part = self[shift..length - 1]
+    second_part.append(first_part).flatten
+  end
+
+  def lab_rotate!(shift = 1)
+    shift %= length
+    first_part = self[0..shift - 1]
+    second_part = self[shift..length - 1]
+    replace(second_part.append(first_part).flatten)
+  end
+end
