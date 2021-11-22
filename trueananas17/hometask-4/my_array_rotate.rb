199e12b20b8a175d03dfd56567f251379ef289ed
trueananas17 <trueananas17@gmail.com>
2021-11-23 01:15:41 +0300

    Fix rubocop violations

--- trueananas17/hometask-4/my_array_rotate.rb
+++ trueananas17/hometask-4/my_array_rotate.rb
@@ -6,6 +6,7 @@
 class Array
   def lab_rotate(shift = 1)
     return if empty?
+    
     shift %= length
     first_part = self[0..shift - 1]
     second_part = self[shift..length - 1]
@@ -14,6 +15,7 @@ class Array
 
   def lab_rotate!(shift = 1)
     return if empty?
+
     shift %= length
     first_part = self[0..shift - 1]
     second_part = self[shift..length - 1]
