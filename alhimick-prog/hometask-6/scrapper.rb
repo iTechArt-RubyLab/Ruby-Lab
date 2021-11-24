7a2c9c798bcee0256d9595a175b9b1b90fec26e3
alhimick-prog <alhimick96@yahoo.com>
2021-11-24 20:29:58 +0300

    needed_gems

--- alhimick-prog/hometask-6/scrapper.rb
+++ alhimick-prog/hometask-6/scrapper.rb
@@ -3,11 +3,14 @@
 
 require 'capybara'
 require 'capybara/dsl'
+require 'capybara/rspec'
 require 'rspec'
+require 'cucumber'
+require 'selenium-webdriver'
 
 RSpec.describe 'Onliner' do
   it 'start page of onliner' do
     visit 'https://www.onliner.by/'
-    expect(page).to have_content('каталог')
+    expect(page).to have_content('whatineed')
   end
 end
