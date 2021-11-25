38c1da9847212b9154792d47191312b5d6b2e880
skripchenkoveta <skripchenkoveta@gmail.com>
2021-11-25 12:53:19 +0300

    fix delete error

--- skripchenkoveta/hometask-5/4-8.sql
+++ skripchenkoveta/hometask-5/4-8.sql
@@ -1,1 +1,6 @@
-DELETE FROM film CASCADE WHERE title = 'The Great Gatsby'; 
+DELETE FROM film_actor WHERE film_id in (SELECT film_id FROM film WHERE title = 'African Egg');
+DELETE FROM film_category WHERE film_id in (SELECT film_id FROM film WHERE title = 'African Egg');
+DELETE FROM payment WHERE rental_id in (SELECT rental_id FROM rental WHERE inventory_id in (SELECT inventory_id FROM inventory WHERE film_id in (SELECT film_id FROM film WHERE title = 'African Egg')));
+DELETE FROM rental WHERE inventory_id in (SELECT inventory_id FROM inventory WHERE film_id in (SELECT film_id FROM film WHERE title = 'African Egg'));
+DELETE FROM inventory WHERE film_id in (SELECT film_id FROM film  WHERE title = 'African Egg');
+DELETE FROM film WHERE title='African Egg';
