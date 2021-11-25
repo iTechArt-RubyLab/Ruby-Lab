15f56762ea9c0911dfa9da889662b72643f81b34
PandaGrom <arsenij.grom@mail.ru>
2021-11-25 12:23:52 +0300

    add rspec tests for myhash

---
+++ PandaGrom/hometask-6/scrapper.rb
@@ -0,0 +1,6 @@
+require 'selenium-webdriver'
+caps = Selenium::WebDriver::Remote::Capabilities.chrome
+caps.page_load_strategy='normal'
+
+driver = Selenium::WebDriver.for :chrome, :desired_capabilities => caps
+driver.get('https://www.google.com')
