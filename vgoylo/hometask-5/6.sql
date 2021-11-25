3e853cb8068b0196a8a2db3409698f23c312b173
vgoylo <y_draiv.by@mail.ru>
2021-11-25 14:18:40 +0300

    Refactoring

--- vgoylo/hometask-5/6.sql
+++ vgoylo/hometask-5/6.sql
@@ -1,2 +1,2 @@
 INSERT INTO film (title, description,release_year,language_id,rental_duration,rental_rate,length)
-VALUES ('Taxi','Daniel Morales (Samy Naceri) is a highly talented driver living in Marseille, Franceю', 1998, 1, 7, 2.00, 120);
+VALUES ('Taxi','Daniel Morales (Samy Naceri) is a highly talented driver living in Marseille, France', 1998, 1, 7, 2.00, 120);
--- vgoylo/hometask-5/7.sql
+++ vgoylo/hometask-5/7.sql
@@ -1,4 +1,3 @@
-DROP DATABASE IF EXISTS shop;
 UPDATE film
 SET title = 'Taxi 2', release_year = 2002
 WHERE film_id = 1002;
