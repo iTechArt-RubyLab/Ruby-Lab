69be9d101eb167e87a4d8ec06f4c4a8fb9d7b953
alhimick-prog <alhimick96@yahoo.com>
2021-11-26 15:15:34 +0300

    work_in_progress

--- alhimick-prog/hometask-6/scrapper.rb
+++ alhimick-prog/hometask-6/scrapper.rb
@@ -4,13 +4,45 @@
 require 'capybara'
 require 'capybara/dsl'
 require 'capybara/rspec'
+require 'capybara/mechanize'
 require 'rspec'
 require 'cucumber'
 require 'selenium-webdriver'
 
+=begin
+Capybara.default_driver = :selenium
+Capybara.default_driver = :webkit
+Capybara.run_server = false
+Capybara.current_driver = :selenium
+caps = Selenium::WebDriver::Remote::Capabilities.chrome
+caps.page_load_strategy = 'normal'
+Capybara.app_host = 'https://www.onliner.by/'
+
+Capybara.register_driver :mechanize do |app|
+  Capybara::Mechanize::Driver.new(proc {})
+end
+session = Capybara::Session.new :mechanize
+session.visit 'https://www.onliner.by/'
+=end
+class OpenOnliner
+  include Capybara::DSL
+  Capybara.register_driver :mechanize do |app|
+    Capybara::Mechanize::Driver.new(proc {})
+  end
+  session = Capybara::Session.new :mechanize
+  session.visit('https://www.onliner.by/')
+
+end
+
+test = OpenOnliner.new
+puts test
+puts test.page
+
+#visit('https://www.onliner.by/')
+=begin
 RSpec.describe 'Onliner' do
   it 'start page of onliner' do
     visit 'https://www.onliner.by/'
     expect(page).to have_content('whatineed')
   end
-end
+=end
