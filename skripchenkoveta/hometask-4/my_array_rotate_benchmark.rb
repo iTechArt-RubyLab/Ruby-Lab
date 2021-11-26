c667e0e35fda79017fd29d74c3ca40201ad37cf5
skripchenkoveta <skripchenkoVeta@gmail.com>
2021-11-26 23:15:19 +0300

    rewrite benchmark files

--- skripchenkoveta/hometask-4/my_array_rotate_benchmark.rb
+++ skripchenkoveta/hometask-4/my_array_rotate_benchmark.rb
@@ -3,28 +3,62 @@
 require 'benchmark'
 require './my_array_rotate'
 
+# rubocop:disable Metrics/BlockLength
 Benchmark.bm do |x|
-  puts 'Lab_rotate!/rotate! Array size are 10 items'
-  x.report { (1..10).to_a.rotate!(4) }
-  x.report { (1..10).to_a.rotate!(7) }
-  x.report { (1..10).to_a.lab_rotate!(4) }
-  x.report { (1..10).to_a.lab_rotate!(7) }
+  @average_real = 0
+  puts 'rotate for an array of 10 elements'
+  10.times do
+    @average_real += x.report { (1..10).to_a.rotate(4) }.real
+  end
+  puts "Average 'real' : #{@average_real / 10}"
 
-  puts 'Lab_rotate!/rotate! Array size are 1000 items'
-  x.report { (1..1000).to_a.rotate!(25) }
-  x.report { (1..1000).to_a.rotate!(99) }
-  x.report { (1..1000).to_a.lab_rotate!(25) }
-  x.report { (1..1000).to_a.lab_rotate!(99) }
+  @average_real = 0
+  puts 'lab_rotate for an array of 10 elements'
+  10.times do
+    @average_real += x.report { (1..10).to_a.lab_rotate(4) }.real
+  end
+  puts "Average 'real' : #{@average_real / 10}"
 
-  puts 'Lab_rotate!/rotate Array size are 10 items'
-  x.report { (1..10).to_a.rotate(4) }
-  x.report { (1..10).to_a.rotate(7) }
-  x.report { (1..10).to_a.lab_rotate(4) }
-  x.report { (1..10).to_a.lab_rotate(7) }
+  @average_real = 0
+  puts 'rotate! for an array of 10 elements'
+  10.times do
+    @average_real += x.report { (1..10).to_a.rotate!(4) }.real
+  end
+  puts "Average 'real' : #{@average_real / 10}"
 
-  puts 'Lab_rotate!/rotate! Array size are 1000 items'
-  x.report { (1..1000).to_a.rotate(10) }
-  x.report { (1..1000).to_a.rotate(99) }
-  x.report { (1..1000).to_a.lab_rotate(10) }
-  x.report { (1..1000).to_a.lab_rotate(99) }
+  @average_real = 0
+  puts 'lab_rotate! for an array of 10 elements'
+  10.times do
+    @average_real += x.report { (1..10).to_a.lab_rotate!(4) }.real
+  end
+  puts "Average 'real' : #{@average_real / 10}"
+
+  @average_real = 0
+  puts 'rotate for an array of 10000 elements'
+  10.times do
+    @average_real += x.report { (1..10_000).to_a.rotate(500) }.real
+  end
+  puts "Average 'real' : #{@average_real / 10}"
+
+  @average_real = 0
+  puts 'lab_rotate for an array of 10000 elements'
+  10.times do
+    @average_real += x.report { (1..10_000).to_a.lab_rotate(500) }.real
+  end
+  puts "Average 'real' : #{@average_real / 10}"
+
+  @average_real = 0
+  puts 'rotate! for an array of 10000 elements'
+  10.times do
+    @average_real += x.report { (1..10_000).to_a.rotate!(500) }.real
+  end
+  puts "Average 'real' : #{@average_real / 10}"
+
+  @average_real = 0
+  puts 'lab_rotate! for an array of 10000 elements'
+  10.times do
+    @average_real += x.report { (1..10_000).to_a.lab_rotate!(500) }.real
+  end
+  puts "Average 'real' : #{@average_real / 10}"
 end
+# rubocop:enable Metrics/BlockLength
--- skripchenkoveta/hometask-4/my_hash_benchmark.rb
+++ skripchenkoveta/hometask-4/my_hash_benchmark.rb
@@ -2,29 +2,64 @@
 
 require 'benchmark'
 require './my_hash'
-
+# rubocop:disable Metrics/BlockLength
 Benchmark.bm do |x|
-  puts 'Hash: 100 and 1000 items'
   hash = {}
-  x.report { 100.times { |el| hash[el] = el } }
-  x.report { hash.delete(6) }
-  x.report { hash.length }
-  x.report { hash.clear }
-  puts '1000 items'
-  x.report { 1000.times { |el| hash[el] = el } }
-  x.report { hash.delete(65) }
-  x.report { hash.length }
-  x.report { hash.clear }
-
-  puts 'MyHash: 100 items'
   my_hash = MyHash.new
-  x.report { 100.times { |el| my_hash[el] = el } }
-  x.report { my_hash.delete(6) }
-  x.report { my_hash.clear }
-  x.report { my_hash.length }
-  puts '1000 items'
-  x.report { 1000.times { |el| my_hash[el] = el } }
-  x.report { my_hash.delete(65) }
-  x.report { my_hash.clear }
-  x.report { my_hash.length }
+  @average_real = 0
+
+  puts 'Creating and writing values in a hash of 100 elements'
+  10.times do
+    @average_real += x.report { 100.times { |el| hash[el] = el } }.real
+  end
+  puts "Average 'real' : #{@average_real / 10}"
+
+  @average_real = 0
+  puts 'Creating and writing values in a my_hash of 100 elements'
+  10.times do
+    @average_real += x.report { 100.times { |el| my_hash[el] = el } }.real
+  end
+  puts "Average 'real' : #{@average_real / 10}"
+
+  @average_real = 0
+  puts 'Reading values from a hash of 100 elements '
+  10.times do
+    @average_real += x.report { 100.times { |el| hash[el] } }.real
+  end
+  puts "Average 'real' : #{@average_real / 10}"
+
+  @average_real = 0
+  puts 'Reading values from a my_hash of 100 elements '
+  10.times do
+    @average_real += x.report { 100.times { |el| my_hash[el] } }.real
+  end
+  puts "Average 'real' : #{@average_real / 10}"
+
+  puts 'Creating and writing values in a hash of 10000 elements'
+  10.times do
+    @average_real += x.report { 10_000.times { |el| hash[el] = el } }.real
+  end
+  puts "Average 'real' : #{@average_real / 10}"
+
+  @average_real = 0
+  puts 'Creating and writing values in a my_hash of 10000 elements'
+  10.times do
+    @average_real += x.report { 10_000.times { |el| my_hash[el] = el } }.real
+  end
+  puts "Average 'real' : #{@average_real / 10}"
+
+  @average_real = 0
+  puts 'Reading values from a hash of 10000 elements '
+  10.times do
+    @average_real += x.report { 10_000.times { |el| hash[el] } }.real
+  end
+  puts "Average 'real' : #{@average_real / 10}"
+
+  @average_real = 0
+  puts 'Reading values from a my_hash of 10000 elements '
+  10.times do
+    @average_real += x.report { 10_000.times { |el| my_hash[el] } }.real
+  end
+  puts "Average 'real' : #{@average_real / 10}"
 end
+# rubocop:enable Metrics/BlockLength
