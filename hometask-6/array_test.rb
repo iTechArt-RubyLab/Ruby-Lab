57e718434f282e8d27477783b36372562090ce81
trueananas17 <trueananas17@gmail.com>
2021-11-28 22:29:09 +0300

    Create files

---
+++ hometask-6/array_test.rb
@@ -0,0 +1,38 @@
+require 'rspec/autorun'
+require_relative 'my_array_rotate'
+
+RSpec.describe Array do
+  describe '#lab_rotate' do
+    context 'whe array is empty' do
+      let(:array) { [] }
+      it 'returns nil' do
+        expect(array.lab_rotate).to eq(nil)
+      end
+    end
+
+    context 'when array contains values' do
+      let(:array) { [1, 2, 3] }
+      it 'returns rotated array without changing it' do
+        expect(array.lab_rotate).to eq([2, 3, 1])
+        expect(array).to eq([1, 2, 3])
+      end
+    end
+  end
+
+  describe '#lab_rotate!' do
+    context 'whe array is empty' do
+      let(:array) { [] }
+      it 'returns nil' do
+        expect(array.lab_rotate!).to eq(nil)
+      end
+    end
+
+    context 'when array contains values' do
+      let(:array) { [1, 2, 3] }
+      it 'returns rotated array ' do
+        expect(array.lab_rotate!).to eq([2, 3, 1])
+        expect(array).to eq([2, 3, 1])
+      end
+    end
+  end
+end
