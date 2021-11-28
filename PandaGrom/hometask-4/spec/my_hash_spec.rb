34ff4321ac967fb00ffa79b0d6b1b9690813d958
PandaGrom <arsenij.grom@mail.ru>
2021-11-28 23:19:31 +0300

    refactoring code for hometask-4 and add scrapper for onliner

--- PandaGrom/hometask-4/spec/my_hash_spec.rb
+++ PandaGrom/hometask-4/spec/my_hash_spec.rb
@@ -3,6 +3,7 @@
 require '../my_hash'
 
 # rubocop:disable Metrics/BlockLength
+
 describe MyHash do
   let(:hash) { described_class.new }
   before do
@@ -10,24 +11,29 @@ describe MyHash do
     hash['world'] = 2
     hash[4237] = 'code'
   end
-  describe 'creating a new pair' do
-    it 'should assign values to the keys and show the size of the array according to the number of values' do
+  describe '#=[]' do
+    it 'assigns values to the keys and show the size of the array according to the number of values' do
       expect(hash['hello']).to eq 1
       expect(hash['world']).to eq 2
       expect(hash[4237]).to eq 'code'
+    end
+  end
+
+  describe '#[]' do
+    it 'output the correct key values' do
+      expect(hash['hello']).to eq 1
+      expect(hash['world']).to eq 2
+      expect(hash[4237]).to eq 'code'
+    end
+  end
+
+  describe '#size' do
+    it 'show hash size' do
       expect(hash.size).to eq 3
     end
   end
 
-  describe 'searching for a value by key' do
-    it 'should output the correct key values' do
-      expect(hash['hello']).to eq 1
-      expect(hash['world']).to eq 2
-      expect(hash[4237]).to eq 'code'
-    end
-  end
-
-  describe 'deleting all elements' do
+  describe '#delete_all' do
     it 'should show a clean hash' do
       expect(hash.delete_all.size).to eq 0
     end
@@ -37,8 +43,9 @@ describe MyHash do
     it 'should return the correct items in the hash' do
       expect(hash.array).to contain_exactly('world', 'hello', 1, 2, 4237, 'code')
     end
-
-    it 'negative scripts' do
+  
+  describe 'negative situations'
+    it 'return false' do
       expect(hash.array.include?('arr')).to eq false
       expect(hash.array.empty?).to eq false
     end
