4e60035d34ad01022bc1969d5b7e8a03f21c26ee
Phoenix#^# <abasovwork@gmail.com>
2021-11-29 00:03:06 +0300

    hound fixl

--- Gek62/hometask-6/scrapper.rb
+++ Gek62/hometask-6/scrapper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'watir'
 require 'webdrivers'
 require 'nokogiri'
@@ -7,64 +9,61 @@ browser = Watir::Browser.new
 browser.goto 'https://onliner.by'
 parsed_page = Nokogiri::HTML(browser.html)
 
-File.open("parsed.txt", "w") { |f| f.write "#{parsed_page}" }
+File.open('parsed.txt', 'w') { |f| f.write parsed_page.to_s }
 
 browser.close
 
-CSV.open("mainblock.csv", "a+") do |csv|
-    csv << ["title", "link", "picture_link"]
+CSV.open('mainblock.csv', 'a+') do |csv|
+  csv << %w[title link picture_link]
 
-    article_cards = parsed_page.xpath("//*[@id='container']/div/div/div/div/div[2]")
-    article_cards.each do |card|
+  article_cards = parsed_page.xpath("//*[@id='container']/div/div/div/div/div[2]")
+  article_cards.each do |card|
+    title = card.xpath('//a/h3/span').text
+    link = card.xpath('//@href')
+    picture = card.xpath('//*[@id]/div/picture')
 
-        title = card.xpath("//a/h3/span").text
-        link = card.xpath("//@href")
-        picture = card.xpath("//*[@id]/div/picture")
-
-        csv << [title, link, picture]
-end
+    csv << [title, link, picture]
+  end
 end
 
 browser = Watir::Browser.new
 browser.goto 'https://catalog.onliner.by'
 parsed_page = Nokogiri::HTML(browser.html)
 
-File.open("parsed.txt", "w") { |f| f.write "#{parsed_page}" }
+File.open('parsed.txt', 'w') { |f| f.write parsed_page.to_s }
 
 browser.close
 
-CSV.open("catalog.csv", "a+") do |csv|
-    csv << ["title", "link", "picture_link"]
+CSV.open('catalog.csv', 'a+') do |csv|
+  csv << %w[title link picture_link]
 
-    article_cards = parsed_page.xpath("//*[@id='container']/div/div/div/div/div[4]")
-    article_cards.each do |card|
+  article_cards = parsed_page.xpath("//*[@id='container']/div/div/div/div/div[4]")
+  article_cards.each do |card|
+    title = card.xpath("//*[@id='container']/div/div/div/div/div/div/div/h3").text
+    link = card.xpath("//*[@id='container']/div/div/div/div/div[2]/div[1]/div[1]/div[1]/@href")
+    picture = card.xpath('//div/picture/img/@src')
 
-        title = card.xpath("//*[@id='container']/div/div/div/div/div/div/div/h3").text
-        link = card.xpath("//*[@id='container']/div/div/div/div/div[2]/div[1]/div[1]/div[1]/@href")
-        picture = card.xpath("//div/picture/img/@src")
-
-        csv << [title, link, picture]
-end
+    csv << [title, link, picture]
+  end
 end
 
 browser = Watir::Browser.new
 browser.goto 'https://money.onliner.by'
 parsed_page = Nokogiri::HTML(browser.html)
 
-File.open("parsed.txt", "w") { |f| f.write "#{parsed_page}" }
+File.open('parsed.txt', 'w') { |f| f.write parsed_page.to_s }
 
 browser.close
 
-CSV.open("money.csv", "a+") do |csv|
-    csv << ["title", "link", "picture_link"]
+CSV.open('money.csv', 'a+') do |csv|
+  csv << %w[title link picture_link]
 
-    article_cards = parsed_page.xpath("//*[@id='container']/div/div/div/div/div[4]")
-    article_cards.each do |card|
+  article_cards = parsed_page.xpath("//*[@id='container']/div/div/div/div/div[4]")
+  article_cards.each do |card|
+    title = card.xpath("//*[@id='container']/div/div/div/div/div/div/div/h3").text
+    link = card.xpath("//*[@id='container']/div/div/div/div/div[2]/div[1]/div[1]/div[1]/@href")
+    picture = card.xpath('//div/picture/img/@src')
 
-        title = card.xpath("//*[@id='container']/div/div/div/div/div/div/div/h3").text
-        link = card.xpath("//*[@id='container']/div/div/div/div/div[2]/div[1]/div[1]/div[1]/@href")
-        picture = card.xpath("//div/picture/img/@src")
-
-        csv << [title, link, picture]
-end
+    csv << [title, link, picture]
+  end
 end
