b2000cb4c5e0877f200bdc4d853aaab5d994be66
mlshvch <malashevichdev@gmail.com>
2021-11-28 23:34:20 +0300

    Add news parsing.

--- mlshvch/hometask-6/scrapper.rb
+++ mlshvch/hometask-6/scrapper.rb
@@ -7,16 +7,23 @@ require 'capybara/dsl'
 require 'selenium-webdriver'
 
 SCRAPING_WEBSITE = 'https://www.onliner.by'
-NEWS_CSS_SELECTOR = ''
-CATALOG_CSS_SELECTOR = 'div.catalog-offers'
+NEWS_CSS_SELECTORS = %w[widget-1-1 widget-2-1 widget-3-1 widget-4-1 widget-5-1 widget-6-1 widget-7-1 widget-8-1
+                        widget-9-1 widget-10-1 widget-11-1 widget-12-1].freeze
+CATALOG_CSS_SELECTOR = 'div.catalog-offers__item'
 PEOPLE_NEWS_SELECTOR = 'ul.b-opinions-main-2'
 # class for parsing webpage data
 class Scraper
   include Capybara::DSL
 
+  attr_reader :result
+
   def initialize
-    @result = {}
+    @result = []
     configure_capybara
+  end
+
+  def show
+    @result
   end
 
   def configure_capybara
@@ -35,7 +42,31 @@ class Scraper
     @html = Nokogiri::HTML(driver.page_source)
   end
 
-  def extract_data(css_selector)
-    @html.search(".#{css_selector}")
+  def parse_news(block)
+    title = block.search('.b-tile-header').text.strip
+    picture = block.at_css('img')
+    picture = !picture.nil? ? picture.attr('src') : block.search('i.b-tile-bg').attr('style').text[21...-1]
+    text = !block.search('.b-tile-excerpt').nil? ? block.search('.b-tile-excerpt').text : ''
+    [title, picture, text]
   end
+
+  def extract_news_data
+    NEWS_CSS_SELECTORS.each do |selector|
+      block = @html.search("div##{selector}")
+      @result << parse_news(block)
+    end
+  end
+
+  def extract_catalog_data
+    @html.search(CATALOG_CSS_SELECTOR)
+  end
+
+  def scrape_data
+    extract_html if @html.nil?
+    extract_catalog_data
+  end
+
 end
+
+scrap = Scraper.new
+scrap.scrape_data
