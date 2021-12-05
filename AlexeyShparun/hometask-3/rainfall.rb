1001e9d8c56d23d09180087ca76f218aa91c9ce9
Ubuntu 20.04 <an.shparun@yandex.by>
2021-12-05 14:10:12 -0800

    performed with the use of classes, removed the dependency of methods

--- AlexeyShparun/hometask-3/rainfall.rb
+++ AlexeyShparun/hometask-3/rainfall.rb
@@ -1,48 +1,51 @@
 #!/usr/bin/env ruby
-
 # frozen_string_literal: true
 
-def upload_data
-  File.read('data.txt')
-end
+# this class is used for calculate mean and variance values
+class Rainfall
+  def initialize(town, data)
+    search = data.split("\n").find { |current_string| current_string.include? town }.to_s
+    @average = search.scan(/\d*\.\d/).map(&:to_f)
+    @mean = @average.sum / 12
+  end
 
-def string_search(town, string)
-  city = string.split("\n").find { |current_string| current_string.include? town }
-  return if city.nil?
+  def mean
+    return -1 if @average.empty?
 
-  city.scan(/\d*\.\d/).map(&:to_f)
-end
+    @mean
+  end
 
-def mean(town, string)
-  @average = string_search(town, string)
-  return -1 if @average.nil?
+  def variance
+    return -1 if @average.empty?
 
-  @mean = @average.sum / 12
-end
-
-def variance
-  return -1 if @average.nil?
-
-  @average.map { |value| (@mean - value)**2 }.sum / 12
-end
-
-def run_cli
-  loop do
-    print 'Enter city: '
-    input = gets.chomp
-    break if input == 'exit!'
-
-    if input.empty?
-      puts 'Empty! Try again'
-    else
-      result(input)
-    end
+    @average.map { |value| (@mean - value)**2 }.sum / 12
   end
 end
 
-def result(input)
-  data = upload_data
-  puts "Mean: #{mean(input, data)}\nVariance: #{variance}"
+# this class is used to input and output values
+class Cli
+  def data
+    File.read('data.txt')
+  end
+
+  def run_cli
+    loop do
+      print 'Enter city: '
+      input = gets.chomp
+      break if input == 'exit!'
+
+      if input.empty?
+        puts 'Empty! Try again'
+      else
+        result(input)
+      end
+    end
+  end
+
+  def result(input)
+    statistics = Rainfall.new(input, data)
+    puts "Mean: #{statistics.mean}\nVariance: #{statistics.variance}"
+  end
 end
 
-run_cli
+Cli.new.run_cli
