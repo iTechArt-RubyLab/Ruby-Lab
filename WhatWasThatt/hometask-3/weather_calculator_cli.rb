1c266979957882b8152710f841ef7b96ac8d2fd3
WhatWasThatt <sashakorol584@mail.ru>
2021-12-10 18:49:02 +0200

    remove weather_calculator_cli

--- WhatWasThatt/hometask-3/weather_calculator_cli.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# Service to download ftp files from the server
-class WeatherCalculatorCli
-  def initialize; end
-end
-
-WeatherCalculatorCli.new('data.txt').run_cli
