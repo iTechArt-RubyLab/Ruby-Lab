fae9e9fa10c91ff8467bff22f5c6b0c8b3202c10
VALIKKKKK <kashko2015v@mail.ru>
2021-12-16 00:38:26 +0300

    decomposite methods

--- VALIKKKKK/hometask-4/my_array_rotate.rb
+++ VALIKKKKK/hometask-4/my_array_rotate.rb
@@ -14,12 +14,22 @@ class Array
 
   def lab_rotate_logic(arr, num)
     if num.positive?
-      if num > arr.size
-        arr.concat(arr.shift(num - arr.size))
-      else
-        arr.concat(arr.shift(num))
-      end
-    elsif (-num) > arr.size
+      positive_case(arr, num)
+    else
+      negative_case(arr, num)
+    end
+  end
+
+  def positive_case(arr, num)
+    if num > arr.size
+      arr.concat(arr.shift(num - arr.size))
+    else
+      arr.concat(arr.shift(num))
+    end
+  end
+
+  def negative_case(arr, num)
+    if (-num) > arr.size
       arr.unshift(arr.slice!(arr.size + num, -num - arr.size)).flatten(1)
     else
       arr.unshift(arr.slice!(num, -num)).flatten(1)
