b5cee3775611d30bd85e15fb57d9f66b5fb52b6a
DaniilGulyuta <danalexs27@gmail>
2021-12-16 16:43:43 +0300

    twill11/hometask-5:adding some changes

--- twill11/hometask-5/4.6.sql
+++ twill11/hometask-5/4.6.sql
@@ -1,3 +1,2 @@
 INSERT INTO film(title, description, language_id, release_year, length)
 VALUES('Sherlock Holmes', 'The greatest detective in history, Sherlock Holmes, together with his loyal ally Watson, enter into a battle that requires serious physical and mental preparation, because their enemy poses a threat to the whole of London.', 1, 2009, 158);
- 
--- twill11/hometask-5/4.8.sql
+++ twill11/hometask-5/4.8.sql
@@ -39,13 +39,13 @@ DELETE FROM rental WHERE inventory_id IN (
    SELECT film_id FROM films_ids
   )
 );
-WITH films_ids AS (
-    SELECT film_id
-    FROM film
-    WHERE title = 'Samurai Lion'
-)
 DELETE FROM inventory WHERE film_id IN (
- SELECT film_id FROM films_ids
+  SELECT film.film_id FROM film
+   INNER JOIN film_category ON film.film_id = film_category.film_id
+    INNER JOIN category ON film_category.category_id = category.category_id
+     WHERE film.title = 'Samurai Lion'
+);
+ 
 );
 DELETE FROM film 
 WHERE title = 'Samurai Lion';
