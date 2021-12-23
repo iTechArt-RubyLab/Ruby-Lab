4ccb63e08eab3e3129e3dc1d7e7b4a1f2b53b460
WhatWasThatt <sashakorol584@mail.ru>
2021-12-23 17:12:01 +0200

    add common main class

---
+++ WhatWasThatt/hometask-6/lib/run_cli.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+require_relative 'characters/processor'
+require_relative 'deaths/processor'
+
+class RunCli
+  def run_cli
+    run
+  end
+
+  private
+
+  attr_accessor :name
+
+  EXIT_COMMANDS = %w[exit! !!! exit].freeze
+
+  def run
+    loop do
+      info_message
+      read_character
+      break if exit?
+
+      death_message
+    end
+  end
+
+  def exit?
+    EXIT_COMMANDS.include?(name)
+  end
+
+  def read_character
+    @name = gets.chomp
+  end
+
+  def info_message
+    puts 'Enter name of character: '
+  end
+
+  def death_message
+    puts(death_info)
+  end
+
+  def characters_hash
+    Characters::Processor.new(:csv, 'characters').call
+  end
+
+  def death_info
+    Deaths::Processor.new(characters_hash, name, :csv, "#{name.split(' ')[0]}_death").call
+  end
+end
+
+RunCli.new.run_cli
