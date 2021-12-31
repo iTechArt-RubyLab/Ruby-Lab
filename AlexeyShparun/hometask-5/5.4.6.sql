41d54c383335f8729227017b03a0ec228a6968b6
Ubuntu 20.04 <an.shparun@yandex.by>
2021-12-31 03:58:56 -0800

    upload hometask-5

---
+++ AlexeyShparun/hometask-5/5.4.6.sql
@@ -0,0 +1,2 @@
+INSERT INTO film (title, description,release_year,language_id) 
+VALUES ('Secret Window', 'Secret Window is a 2004 American psychological horror thriller film starring Johnny Depp and John Turturro.',2004,1);
